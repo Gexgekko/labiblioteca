2655ddfbab22862af592b3cfc6525cddef5cadd5
Gexgekko <scgexx@gmail.com>
2015-05-05 19:26:26 +0200

    Reinicio repositorio

---
+++ README.md
@@ -0,0 +1,5 @@
+# La Biblioteca
+
+Fue inicialmente escrito en Batch como ideilla, pero he decidido intentarlo en una versión en AngularJS con menos complicaciones.
+
+En la el fichero "batch" meteré el original (con contraseña, aunque es solo un boceto/prueba).
---
+++ ang/app.js
@@ -0,0 +1,16 @@
+var laBibliotecaApp = angular.module('laBibliotecaApp',[]);
+
+laBibliotecaApp.controller('baseCtrl', ['$scope','$interval', function($scope,$interval){
+	var vm = $scope;
+	vm.txtcentral = 0;
+	$scope.daEnter = function(keyEvent,bloque,parr) {
+ 		if (keyEvent.which === 13){
+ 			console.log(vm.nombreJugador);
+ 			vm.txtcentral = bloque;
+ 			vm.parrafo = 0;
+ 			$interval(function(){
+ 				vm.parrafo = vm.parrafo + 1;
+ 			}, 5000, parr);
+ 		}
+	}
+}])
