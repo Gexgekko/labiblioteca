0726463eebdc9878e1337f74b331b72271f9e9ba
Gexgekko <scgexx@gmail.com>
2015-05-06 18:04:59 +0200

    Nuevo keypress

--- ang/app.js
+++ ang/app.js
@@ -1,27 +1,5 @@
 var laBibliotecaApp = angular.module('laBibliotecaApp',[]);
 
-laBibliotecaApp.config(
-	function keyEventsConfig( keyEventsProvider ) {
-
-			// Each shortcut() operator gets called with the event object and the 
-			// "is" object. The goal here is to add ".is" properties based on the 
-			// state of the event.
-			keyEventsProvider.addShortcut(
-				function operator( event, is ) {
-					is.esc = ( ( event.type === "keydown" ) && ( event.which === 27 ) );
-					// NOTE: The CMD-ENTER combination is more consistently reported
-					// in the keydown event (as opposed to the keypress event). As 
-					// such, we'll consider it false unless the event type is appropriate.
-					is.cmdEnter = ( ( event.type === "keydown" ) && ( event.which === 13 ) && event.metaKey );
-					if(is.cmdEnter){
-						console.log('Es enter');
-					}
-				}
-			);
-		}
-);
-
-
 laBibliotecaApp.controller('baseCtrl', ['$scope','$interval', function($scope,$interval){
 	var vm = $scope;
 	vm.txtcentral = 0;
