201aeaaf6812116f50b1903c32de5ab22dd12ba3
Gexgekko <scgexx@gmail.com>
2015-05-08 16:27:38 +0200

    Fix2

--- pruebasNG/objInv/ang/app.js
+++ pruebasNG/objInv/ang/app.js
@@ -6,6 +6,7 @@ pruebaInvApp.controller('invCtrl', ['$scope','$interval', '$http', '$cookieStore
 		vm.objetos = objeto;
 		console.log(vm.objetos);
 	})
+	vm.player = [];
 	vm.player.inventario = [];
 	vm.equipar = function(){
 		var equipar = { "cabeza": vm.equipCabeza, "brazoIzq": vm.equipBrazoIzq, "brazoDer": vm.equipBrazoDer };
