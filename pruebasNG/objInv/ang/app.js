d25d727eda80027fb6956702e7fbd4a5cd7694b4
Gexgekko <scgexx@gmail.com>
2015-05-08 16:45:11 +0200

    Logs

--- pruebasNG/objInv/ang/app.js
+++ pruebasNG/objInv/ang/app.js
@@ -10,18 +10,22 @@ pruebaInvApp.controller('invCtrl', ['$scope','$interval', '$http', '$cookieStore
 	
 	vm.player = [];
 	var tmpInv =  $cookieStore.get('playInv');
+	console.log("Temporal: ")
 	console.log(tmpInv);
 	if(angular.isObject(tmpInv)){
 		vm.player.inventario = $cookieStore.get('playInv');
 		console.log("Cargado");
+		console.log(vm.player.inventario);
 	}else{
 		vm.player.inventario = [];
 	}
 	vm.equipar = function(){
 		var equipar = { "cabeza": vm.player.cabeza, "brazoIzq": vm.player.brazoIzq, "brazoDer": vm.player.brazoDer };
+		console.log("Equipar: ");
 		console.log(equipar);
 		$cookieStore.put('playInv', equipar);
 		vm.player.inventario = $cookieStore.get('playInv');
+		console.log("Inventario: ");
 		console.log(vm.player.inventario);
 	}
 }])
