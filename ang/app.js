7a72a917fb286287eb139e8dd6e27d55d9725bda
Gexgekko <scgexx@gmail.com>
2015-05-09 17:26:49 +0200

    Sin config, todo base

--- ang/app.js
+++ ang/app.js
@@ -1,17 +1,30 @@
 var laBibliotecaApp = angular.module('laBibliotecaApp',['ngCookies']);
-laBibliotecaApp.config(['$scope','$cookieStore', function($scope,$cookieStore){
-	var vm = $scope;
-//	var tmpPart = $cookieStore.get('playInv');
-//	if(angular.isObject(tmpPart)){
-//		vm.usr = tmpPart;
-//	}else{
-//		vm.usr = [];
-//	}
-	vm.txtcentral = 0;
-}]);
 
 laBibliotecaApp.controller('baseCtrl', ['$scope','$interval','$cookieStore', function($scope,$interval,$cookieStore){
 	var vm = $scope;
+
+	//Config iniciales
+	var tmpPart = $cookieStore.get('playInv');
+	if(angular.isObject(tmpPart)){
+		vm.usr = tmpPart;
+	}else{
+		vm.usr = [];
+	}
+	vm.txtcentral = 0;
+
+	
+	vm.daEnter = function(keyEvent,bloque,parr) {
+ 		if (keyEvent.which === 13){
+ 			vm.txtcentral = bloque;
+ 			vm.parrafo = 0;
+ 			if(parr > 0){
+ 				$interval(function(){
+ 					vm.parrafo = vm.parrafo + 1;
+ 				}, 3000, parr);
+ 			}
+ 		}
+	}
+
 	vm.$watch('usr.clase',function(clase, antiguo){
 		if(clase == 'Mago'){
 			console.log("Ha entrao en mago")
@@ -25,15 +38,5 @@ laBibliotecaApp.controller('baseCtrl', ['$scope','$interval','$cookieStore', fun
 			vm.usr.precision=15
 		}
 	})
-	vm.daEnter = function(keyEvent,bloque,parr) {
- 		if (keyEvent.which === 13){
- 			vm.txtcentral = bloque;
- 			vm.parrafo = 0;
- 			if(parr > 0){
- 				$interval(function(){
- 					vm.parrafo = vm.parrafo + 1;
- 				}, 3000, parr);
- 			}
- 		}
-	}
+
 }])
