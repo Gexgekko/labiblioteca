45411834de3a14233629073f69a7a5c4b015a559
Gexgekko <scgexx@gmail.com>
2015-05-09 16:54:11 +0200

    Stats y mago listos

--- ang/app.js
+++ ang/app.js
@@ -1,9 +1,24 @@
 var laBibliotecaApp = angular.module('laBibliotecaApp',[]);
-
 laBibliotecaApp.controller('baseCtrl', ['$scope','$interval', function($scope,$interval){
+	vm.usr = [];
 	var vm = $scope;
 	vm.txtcentral = 0;
-	$scope.daEnter = function(keyEvent,bloque,parr) {
+	vm.usr.clase = "";
+	vm.$watch('vm.usr.clase', function(clase){
+		if(clase == 'Mago'){
+			vm.usr.magia=20
+			vm.usr.pegar=15
+			vm.usr.golpear=13
+			vm.usr.esquivar=17
+			vm.usr.aguante=13
+			vm.usr.vitalidad=15
+			vm.usr.sigilo=15
+			vm.usr.precision=15
+			console.log('Stats asignadas a usuario: ');
+			console.log(vm.usr);
+		}
+	})
+	vm.daEnter = function(keyEvent,bloque,parr) {
  		if (keyEvent.which === 13){
  			console.log(vm.player.nombre);
  			vm.txtcentral = bloque;
